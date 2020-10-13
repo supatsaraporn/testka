ee050d9c08d899c3ba01c3f98a6ebe13dd8f021a
supatsaraporn <supatsaraporn.wo.61@ubu.ac.th>
2020-10-14 01:19:18 +0700

    Updates

---
+++ precache-manifest.5913209ac6802abae615d6254d7a72ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d55293cb4b15c868d063cafc278fc46c",
+    "url": "/testka/index.html"
+  },
+  {
+    "revision": "cf230826607e8a5ab101",
+    "url": "/testka/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "aa80acd8a6f8393b09b6",
+    "url": "/testka/static/css/main.0236d509.chunk.css"
+  },
+  {
+    "revision": "cf230826607e8a5ab101",
+    "url": "/testka/static/js/2.f0dd6d56.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/testka/static/js/2.f0dd6d56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa80acd8a6f8393b09b6",
+    "url": "/testka/static/js/main.b5971b4e.chunk.js"
+  },
+  {
+    "revision": "fde5201c8f47b0f19ed5",
+    "url": "/testka/static/js/runtime-main.51915dcc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testka/static/media/logo.5d5d9eef.svg"
+  }
+]);
